c7aa7b83a7259e56a76c13a4a5382928101917ac
Lucas Garron <code@garron.net>
2020-10-03 23:20:39 -0700

    Remove `PG3D` workarounds.

--- src/scramble-view/vendor/DisplayablePG3D.ts
+++ src/scramble-view/vendor/DisplayablePG3D.ts
@@ -1,11 +1,11 @@
-import { KPuzzle, KPuzzleDefinition, Puzzles as KPuzzles } from "cubing/kpuzzle";
+import { KPuzzleDefinition, Puzzles as KPuzzles } from "cubing/kpuzzle";
 import { getPuzzleGeometryByDesc, StickerDat } from "cubing/puzzle-geometry";
 import { PuzzleName, Puzzles as PGPuzzles } from "./pg-puzzles";
 
 class DisplayableKPuzzle {
   public type: "kpuzzle" = "kpuzzle";
   // TODO: push display name into the KSolve defition.
-  constructor(private kpuzzleName: string) { }
+  constructor(private kpuzzleName: string) {}
 
   public displayName(): string {
     return KPuzzles[this.kpuzzleName].name;
@@ -26,7 +26,12 @@ class DisplayableKPuzzle {
 
 class DisplayablePG3D {
   public type: "pg3d" = "pg3d";
-  constructor(private displayNameStr: string, private name: PuzzleName, private desc: string, public polarVantages: boolean) { }
+  constructor(
+    private displayNameStr: string,
+    private name: PuzzleName,
+    private desc: string,
+    public polarVantages: boolean
+  ) {}
 
   public displayName(): string {
     return this.displayNameStr;
@@ -39,24 +44,13 @@ class DisplayablePG3D {
   public kpuzzleDefinition(): KPuzzleDefinition {
     const pg = getPuzzleGeometryByDesc(this.desc, ["orientcenters", "true"]);
     const kpuzzleDef = pg.writekpuzzle();
-    const worker = new KPuzzle(kpuzzleDef);
-
-    // Wide move / rotation hack
-    worker.setFaceNames(pg.facenames.map((_: any) => _[1]));
-    const mps = pg.movesetgeos;
-    for (const mp of mps) {
-      const grip1 = mp[0] as string;
-      const grip2 = mp[2] as string;
-      // angle compatibility hack
-      worker.addGrip(grip1, grip2, mp[4] as number);
-    }
     return kpuzzleDef;
   }
 
   public stickerDat(): StickerDat {
     // TODO: Remove `as` cast.
     const pg = getPuzzleGeometryByDesc(this.desc, ["orientcenters", "true"]);
-    return pg.get3d(0.0131);
+    return pg.get3d();
   }
 
   public kPuzzleDefinition(): KPuzzleDefinition {
@@ -78,14 +72,39 @@ const puzzles: { [s: string]: DisplayablePuzzle } = {};
 for (const key in KPuzzles) {
   puzzles[key as any] = new DisplayableKPuzzle(key);
 }
-puzzles.megaminx = new DisplayablePG3D("Megaminx", "megaminx", PGPuzzles.megaminx, false);
+puzzles.megaminx = new DisplayablePG3D(
+  "Megaminx",
+  "megaminx",
+  PGPuzzles.megaminx,
+  false
+);
 puzzles.skewb = new DisplayablePG3D("Skewb", "skewb", PGPuzzles.skewb, true);
-puzzles.fto = new DisplayablePG3D("FTO", "FTO", "o f 0.333333333333333 v -2", true);
+puzzles.fto = new DisplayablePG3D(
+  "FTO",
+  "FTO",
+  "o f 0.333333333333333 v -2",
+  true
+);
 puzzles["2x2x2"] = new DisplayablePG3D("2x2x2", "2x2x2", "c f 0", true);
-puzzles["3x3x3"] = new DisplayablePG3D("3x3x3", "3x3x3", "c f 0.333333333333333", true);
+puzzles["3x3x3"] = new DisplayablePG3D(
+  "3x3x3",
+  "3x3x3",
+  "c f 0.333333333333333",
+  true
+);
 puzzles["4x4x4"] = new DisplayablePG3D("4x4x4", "4x4x4", "c f 0.5 f 0", true);
 puzzles["5x5x5"] = new DisplayablePG3D("5x5x5", "5x5x5", "c f 0.6 f 0.2", true);
-puzzles["6x6x6"] = new DisplayablePG3D("6x6x6", "6x6x6", "c f 0.666666666666667 f 0.333333333333333 f 0", true);
-puzzles["7x7x7"] = new DisplayablePG3D("7x7x7", "7x7x7", "c f 0.714285714285714 f 0.428571428571429 f 0.142857142857143", true);
+puzzles["6x6x6"] = new DisplayablePG3D(
+  "6x6x6",
+  "6x6x6",
+  "c f 0.666666666666667 f 0.333333333333333 f 0",
+  true
+);
+puzzles["7x7x7"] = new DisplayablePG3D(
+  "7x7x7",
+  "7x7x7",
+  "c f 0.714285714285714 f 0.428571428571429 f 0.142857142857143",
+  true
+);
 
 export { puzzles };

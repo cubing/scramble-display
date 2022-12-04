2e697c9bf07ac5f6d80b7da88f262cfc86d1af85
Lucas Garron <code@garron.net>
2022-12-04 03:22:08 -0800

    Set build target to ES2020.

--- script/build-bundle-global.js
+++ script/build-bundle-global.js
@@ -3,6 +3,7 @@ import { build } from "esbuild";
 build({
 	entryPoints: ["src/scramble-display/bundle-global.ts"],
 	format: "esm",
+	target: "es2020",
 	outfile: "dist/bundle-global/scramble-display.bundle-global.js",
 	bundle: true,
 	minify: true,
--- script/build-esm.js
+++ script/build-esm.js
@@ -1,9 +1,10 @@
 import { build } from "esbuild";
 
 build({
-  entryPoints: ["src/scramble-display/index.ts"],
-  format: "esm",
-  outdir: "dist/esm",
-  bundle: true,
-  external: ["cubing"],
+	entryPoints: ["src/scramble-display/index.ts"],
+	format: "esm",
+	target: "es2020",
+	outdir: "dist/esm",
+	bundle: true,
+	splitting: true,
 });

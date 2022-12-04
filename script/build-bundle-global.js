4af3583fbfac0411a5d9920a7f477dc28d69e84a
Lucas Garron <code@garron.net>
2022-12-04 03:19:27 -0800

    Fix `bundle-global` file name.

--- script/build-bundle-global.js
+++ script/build-bundle-global.js
@@ -1,9 +1,9 @@
 import { build } from "esbuild";
 
 build({
-  entryPoints: ["src/scramble-display/bundle-global.ts"],
-  format: "esm",
-  outfile: "dist/bundle-global/scramble-display.bundle-global.ts",
-  bundle: true,
-  minify: true,
+	entryPoints: ["src/scramble-display/bundle-global.ts"],
+	format: "esm",
+	outfile: "dist/bundle-global/scramble-display.bundle-global.js",
+	bundle: true,
+	minify: true,
 });
